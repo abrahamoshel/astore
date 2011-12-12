150bb31c1350148c039637d8b8669a1d873004a5
Abraham Oshel <abraham.oshel@gmail.com>
2011-12-12 16:08:36 -0600

    clean again

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -1,3 +1,3 @@
 EMAILFROM = "Company Store <employee@company.com>"
 REPLYTO = "employee@company.com"
-EMAILTOSCHEDULETEAM = "employee@company.com"
+EMAILTOSCHEDULETEAM = "employee@company.com, employee2@apple.com"
